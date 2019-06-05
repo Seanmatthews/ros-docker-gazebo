19d65d9667e82354cf9ccc688ee1b51503b7726e
Sean Matthews <sean.d.matthews@gmail.com>
2019-06-05 17:56:41 -0400

    Initial commit

---
+++ kinetic/launch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+xhost +local:root
+
+docker run -it \
+    --env="DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    -env="XAUTHORITY=$XAUTH" \
+    --volume="$XAUTH:$XAUTH" \
+    --runtime=nvidia \
+    ros-gazebo
+
+xhost -local:root
